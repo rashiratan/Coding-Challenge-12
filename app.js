8637461c6b266a8b3274a6ea643c6fa856344ad7
Rashi Ratan <rashiiratan@gmail.com>
2024-10-27 20:24:54 -0400

    Adding comments for readability

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ const ctx = canvas.getContext('2d');
 const clearCanvas = document.getElementById('clearCanvas');
 const colorPicker = document.getElementById('colorPicker');
 
-
+//creating function to reset canvas
 function resetCanvas(){
     ctx.clearRect(0,0, canvas.width, canvas.height);
     ctx.fillStyle = '#f0f0f0';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-resetCanvas();
+resetCanvas(); //initiliazing reset canvas function
 
 let currentTool = 'line';
 let startX;
@@ -22,15 +22,17 @@ let currentY;
 let currentX;
 let isDrawing = false;
 
+//setting variable to identify current tool
 document.querySelectorAll('.tool').forEach((radio) => {
     radio.addEventListener('change', (event) => {
         currentTool = event.target.value;
     })
 })
 
+//setting up functions for each tool
 function drawLine(x, y, a, b) {
     ctx.beginPath();
-    ctx.strokeStyle = colorPicker.value;
+    ctx.strokeStyle = colorPicker.value; //using colorpicker to set color for drawing
     ctx.lineWidth = 2;
     ctx.moveTo(x,y);
     ctx.lineTo(a,b);
@@ -47,7 +49,7 @@ function drawRectangle(x,y, a, b){
 
 function drawCircle (x, y, a, b) {
     ctx.beginPath();
-    let radius = Math.sqrt(Math.pow(a-x, 2) + Math.pow(b-y, 2));
+    let radius = Math.sqrt(Math.pow(a-x, 2) + Math.pow(b-y, 2)); //using circle formula to find radius from coordinates
     ctx.arc(x, y, radius, 0, 2*Math.PI);
     ctx.strokeStyle = colorPicker.value;
     ctx.lineWidth = 2;
@@ -55,7 +57,7 @@ function drawCircle (x, y, a, b) {
 }
  
  
- canvas.addEventListener('mousedown', event => {
+ canvas.addEventListener('mousedown', event => { //beginning the drawing
      isDrawing = true;
      startX = event.offsetX;
      startY = event.offsetY;
@@ -63,12 +65,13 @@ function drawCircle (x, y, a, b) {
  
 
  canvas.addEventListener('mouseup', event =>
- {
+    //drawing on mouseup to ensure that the draw where the mouse button is lifted up
+ {  //finding the second set of coordinates
      currentX = event.offsetX;
      currentY = event.offsetY;
    //task 3
      if(isDrawing === true) {
-        if(currentTool === 'line') {
+        if(currentTool === 'line') { //executing drawing based on selected tool
             drawLine(startX, startY, currentX, currentY);
         }
         else if (currentTool === 'rectangle') {
@@ -79,13 +82,13 @@ function drawCircle (x, y, a, b) {
         }
     }
  
-     isDrawing = false;
+     isDrawing = false; //ending the drawing
  }
  )
  
  //task 4
 
-
+//setting up the clear canvas button
  clearCanvas.addEventListener('click', () => {
     resetCanvas();
  })
